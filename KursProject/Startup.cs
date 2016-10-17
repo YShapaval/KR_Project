4af032b3ae9b896af37554b82005032d9b01654b
Yaheni Shapaval <YShapaval@gomel.iba.by>
2016-10-17 16:08:45 +0300

    Добавьте файлы проекта.

---
+++ KursProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KursProject.Startup))]
+namespace KursProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
